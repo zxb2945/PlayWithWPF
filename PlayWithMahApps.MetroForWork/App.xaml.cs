57efd44b73a75e4ba096067670ef2e8284522388
zxb2945 <52512353+zxb2945@users.noreply.github.com>
2023-11-24 13:45:18 +0900

    Create a Prism Template

---
+++ PlayWithMahApps.MetroForWork/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using PlayWithMahApps.MetroForWork.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace PlayWithMahApps.MetroForWork
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
